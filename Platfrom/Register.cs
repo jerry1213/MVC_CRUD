659f854286c2fb2350a8b486d8985d274631ace7
Jerry <jerry19941213@gmail.com>
2024-05-25 00:52:19 +0800

    Platfrom加上Register

---
+++ Platfrom/Register.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Platform.LogHelper;
+using Platform.Utility;
+using System.Reflection.Emit;
+
+namespace Platform
+{
+    public static class Register
+    {
+        public static void RegisterPlatform(this IServiceCollection service)
+        {
+            service.AddSingleton(typeof(LogService<>));
+        }
+    }
+}
+
